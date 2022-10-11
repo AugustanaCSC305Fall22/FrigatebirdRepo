b7d81d557e441c93966972a6c62e50c86dbc8d72
jwarhank21 <113040694+jwarhank21@users.noreply.github.com>
2022-10-11 01:08:33 -0500

    Removed old code
    This didn't code was out of date

--- Frigatebirdproject/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module edu.augie.Frigatebirdproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens edu.augie.Frigatebirdproject to javafx.fxml;
-    exports edu.augie.Frigatebirdproject;
-}
