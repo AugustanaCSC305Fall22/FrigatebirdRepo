dec646a06121290f5d640090885297cba9dc4b2b
Bill Unyi <36928559+BillUnyi@users.noreply.github.com>
2022-10-25 11:23:53 -0500

    Add files via upload

--- csc305ProjectPreview/src/main/java/frigatebird/ui/MapPreviewController.java
+++ csc305ProjectPreview/src/main/java/frigatebird/ui/MapPreviewController.java
@@ -3,10 +3,11 @@ package frigatebird.ui;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class EditorController {
-
-    @FXML
+public class MapPreviewController {
+	
+	@FXML
     private void switchToMainMenu() throws IOException {
         App.setRoot("MainMenu");
     }
+	
 }
