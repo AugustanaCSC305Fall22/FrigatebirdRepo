0fea0c1e4f32c5f7cd2c0bb4727f8a64c689efb2
Unyi <billunyi21@augustana.edu>
2022-10-25 11:10:41 -0500

    Revert "Fixed Gson implementation issues"
    This reverts commit cf85c911b57cc1f9663580e0e72ecd68d0b6e8ab.

--- csc305ProjectPreview/src/main/java/module-info.java
+++ csc305ProjectPreview/src/main/java/module-info.java
@@ -1,12 +1,7 @@
 module edu.augustana.csc305ProjectPreview {
-	
     requires javafx.controls;
     requires javafx.fxml;
-    requires com.google.gson;
-	requires javafx.graphics;
 
-    opens frigatebird.ui to javafx.fxml;
-    opens frigatebird.terrainbuilder to com.google.gson;
-    exports frigatebird.ui;
-    
+    opens frigatebird.terrainbuilder to javafx.fxml;
+    exports frigatebird.terrainbuilder;
 }
