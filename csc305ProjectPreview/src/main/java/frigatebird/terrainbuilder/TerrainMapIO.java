79519f4ffe4223faa9b8768489de744cd40a2615
Bill Unyi <billunyi21@augustana.edu>
2022-10-25 00:57:05 -0500

    Revert "Added more with Gson"
    This reverts commit 1385346ceb5d2254458ca5b95cac9f44da8d71e8.

--- csc305ProjectPreview/src/main/java/frigatebird/terrainbuilder/TerrainMapIO.java
+++ csc305ProjectPreview/src/main/java/frigatebird/terrainbuilder/TerrainMapIO.java
@@ -1,18 +1,5 @@
 package frigatebird.terrainbuilder;
 
-import com.google.gson.*;
-
 public class TerrainMapIO {
-	
-	public static String terrainMapToJSON(TerrainMap map) {
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		return gson.toJson(map);		
-	}
-	
-	public static TerrainMap jsonToTerrainMap(String jsonText) {
-		Gson gson = new Gson();
-		TerrainMap map = gson.fromJson(jsonText, TerrainMap.class);
-		return map;
-	}
-	
+
 }
