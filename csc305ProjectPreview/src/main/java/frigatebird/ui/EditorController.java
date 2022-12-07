f860d6b0c4818cca6ce6a45f6bdbc44ee260bc56
Beamlak <beamlakabdisa21@augustana.edu>
2022-12-07 03:36:26 -0600

    merged code

--- csc305ProjectPreview/src/main/java/frigatebird/ui/EditorController.java
+++ csc305ProjectPreview/src/main/java/frigatebird/ui/EditorController.java
@@ -109,7 +109,7 @@ public class EditorController {
 			try {
 				handleCanvasClick(e);
 			} catch (IOException e1) {
-				// TODO Auto-generated catch block
+				
 				e1.printStackTrace();
 			}
 		});
